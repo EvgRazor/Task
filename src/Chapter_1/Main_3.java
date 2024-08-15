26d702e05823947cdf676add95528a0bcd0d6cae
EvgRazor <goodevgeni@gmail.com>
2024-08-15 09:34:38 +0300

    task 3

---
+++ src/Chapter_1/Main_3.java
@@ -0,0 +1,14 @@
+package Chapter_1;
+
+public class Main_3 {
+
+    public static void main(String[] args) {
+        String name = "qwertyuio";
+
+        StringBuffer stringBuffer = new StringBuffer(name);
+
+        System.out.println(stringBuffer.reverse());
+
+    }
+
+}

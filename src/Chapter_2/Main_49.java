14f4b420bcf95647d1dbcb888d86c23a7030b591
EvgRazor <goodevgeni@gmail.com>
2024-09-24 09:11:43 +0300

    Task 49

---
+++ src/Chapter_2/Main_49.java
@@ -0,0 +1,13 @@
+package Chapter_2;
+
+public class Main_49 {
+
+    public static void main(String[] args) {
+
+        Main_49_Unmutated main49Unmutated = new Main_49_Unmutated(1.2D,2.3D);
+        System.out.println(main49Unmutated.getA());
+        System.out.println(main49Unmutated.getB());
+
+    }
+
+}

44a50bba1ab5a9b68c3465b0dfd79da53e697a27
Evgeni Hud <e.gud@bft.ru>
2024-12-18 09:56:24 +0300

    88

---
+++ src/leetcode/easy/RemoveDuplicatesFromSortedList_83.java
@@ -0,0 +1,27 @@
+package leetcode.easy;
+
+public class RemoveDuplicatesFromSortedList_83 {
+
+
+/*
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head; // Начинаем с головы списка
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+*/
+
+}
